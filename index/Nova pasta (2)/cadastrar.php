e9721189e439e09ac4b8c95d326ef5c3c67bd176
oliveleonardo0404-ai <oliveleonardo0404@gmail.com>
2026-02-03 14:56:55 -0300

    Add files via upload

--- index/Nova pasta (2)/cadastrar.php
+++ index/Nova pasta (2)/cadastrar.php
@@ -2,16 +2,21 @@
 
 include("connect.php");
 
-$nome = $_POST['Nome'];
+$nome = $_POST['nome'];
 $cpf = $_POST['cpf'];
 $email = $_POST['email'];
-$senha = $_POST['Senha'];
+$senha = $_POST['senha'];
 $Data_nascimento = $_POST['data'];
 
 
 echo $email , $senha , $nome , $cpf , $Data_nascimento;
 
-$sql = "INSERT INTO usuario VALUES ('$email','$senha','$nome','$cpf','$Data_nascimento')";
+$sql = "INSERT INTO usuario(nome, cpf, email, senha, Data_nascimento)
+         VALUES ('$email','$senha','$nome','$cpf','$Data_nascimento')";
+
+$query = mysqli_query($conn, $sql);
+
+
 
 
 
--- index/Nova pasta (2)/connect.php
+++ index/Nova pasta (2)/connect.php
@@ -1,19 +1,13 @@
 <?php
 
-
 $server = "localhost";
 $user = "root";
 $password = "root";
 $database = "aula_php";
 $port = 3307;
 
+$conn = new mysqli($server, $user, $password, $database, $port);
 
-$conn = new mysqli($server,$user,$password,$database,$port);
-
-
-if($conn ->connect_error){
-    die("Erro na conexão com o banco de dados" . $conn->connect_error);
-}
-else{
-    echo("Conectado com sucesso!!");
+if ($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error);
 }
